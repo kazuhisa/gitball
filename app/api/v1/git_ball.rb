e5dc8f363e6ee74f6e2e9090d7a9a24086ccf431
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2015-01-19 21:41:30 +0900

    testing...

--- app/api/v1/git_ball.rb
+++ app/api/v1/git_ball.rb
@@ -2,13 +2,12 @@ class GitBall < Grape::API
   format :json
 
   resource :comment do
-    params do
-      requires :Parameters, type: String
-    end
+    # params do
+    #   requires :Parameters, type: String
+    # end
 
     post '/' do
-      logger.debug(params)
-      PostLog.create(data: params[:Parameters].to_s)
+      PostLog.create(data: params.to_s)
     end
   end
 end
