3fd6528d09230cffed777fef732e8b07cfdb0d4f
kazuhisa <ak.hisashi@gmail.com>
2015-01-23 13:44:52 +0900

    fixed comment languages.

--- app/api/v1/git_ball.rb
+++ app/api/v1/git_ball.rb
@@ -1,15 +1,13 @@
 class GitBall < Grape::API
   format :json
 
-
   resource :comment do
     params do
       requires :payload, type: String
     end
 
     post '/' do
-      PostLog.create(data: params.payload.to_s)
-      Github.update_ball_tag(params.payload.to_s)
+      Github.update_ball_tag(params.payload)
     end
   end
 end
--- lib/github.rb
+++ lib/github.rb
@@ -6,31 +6,31 @@ class Github
       @data = JSON.parse(payload)
     end
 
-    # _ballラベルのリストを取得
+    # Getting '_ball' label lists.
     def ball_tags
       return [] unless @data['action'] == 'created' || @data['action'] == 'opened'
-      if @data['action'] == 'created' && @data.has_key?('issue') #issueとプルリクにコメントが追加された場合
+      if @data['action'] == 'created' && @data.has_key?('issue') # add issue and pullreq comment.
         create_tags_from_message(@data['comment']['body'])
-      elsif @data['action'] == 'opened' && @data.has_key?('issue') #issueが新規作成された場合
+      elsif @data['action'] == 'opened' && @data.has_key?('issue') # create issue.
         create_tags_from_message(@data['issue']['body'])
-      elsif @data['action'] == 'opened' && @data.has_key?('pull_request') # プルリクが新規作成された場合
+      elsif @data['action'] == 'opened' && @data.has_key?('pull_request') # create pullreq.
         create_tags_from_message(@data['pull_request']['body'])
       end
     end
 
-    # issue番号を取得
+    # Getting issue number
     def number
       return nil unless @data['action'] == 'created' || @data['action'] == 'opened'
-      if @data.has_key?('issue') #issueの場合
+      if @data.has_key?('issue') # issue
         @data['issue']['number']
-      elsif @data.has_key?('pull_request') # プルリクの場合
+      elsif @data.has_key?('pull_request') # pullreq
         @data['pull_request']['number']
       else
         nil
       end
     end
 
-    # 書き込み内容からタグを生成
+    # create tags from message.
     def create_tags_from_message(message)
       list = message.scan(/@[a-z0-9_-]+/i)
       list.map { |v| v.sub('@','') + '_ball' }
@@ -53,22 +53,22 @@ class Github
     add_labels_to_an_issue(@payload.number, @payload.ball_tags)
   end
 
-  # issueにラベルを追加する
+  # add label for issue
   def add_labels_to_an_issue(issue_number, label_names)
     @client.add_labels_to_an_issue(@repos, issue_number, label_names)
   end
 
-  # issueに付けられてるラベルを取り外す
+  # remove label from issue
   def remove_label(issue_number, label_name)
     @client.remove_label(@repos, issue_number, label_name)
   end
 
-  # issueに付けられているラベルの一覧を取得する
+  # getting labels from issue
   def labels_for_issue(issue_number)
     @client.labels_for_issue(@repos, issue_number)
   end
 
-  # issueに付けられているballラベルを全て除去する
+  # remove all labels from issue
   def remove_ball_labels(issue_number)
     labes = labels_for_issue(issue_number)
     labes.select { |v| v[:name] =~ /_ball$/ }.each do |label|
