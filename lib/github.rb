b10490ecb483ccd52352bddde0ac4937d11cdbc6
kazuhisa <ak.hisashi@gmail.com>
2015-01-23 11:51:29 +0900

    Add PullRequest message supported.

--- lib/github.rb
+++ lib/github.rb
@@ -1,22 +1,56 @@
 class Github
   require 'octokit'
+
+  class Payload
+    def initialize(payload)
+      @data = JSON.parse(payload)
+    end
+
+    # _ballラベルのリストを取得
+    def ball_tags
+      return [] unless @data['action'] == 'created' || @data['action'] == 'opened'
+      if @data['action'] == 'created' && @data.has_key?('issue') #issueとプルリクにコメントが追加された場合
+        create_tags_from_message(@data['comment']['body'])
+      elsif @data['action'] == 'opend' && @data.has_key?('issue') #issueが新規作成された場合
+        create_tags_from_message(@data['issue']['body'])
+      elsif @data['action'] == 'opend' && @data.has_key?('pull_request') # プルリクが新規作成された場合
+        create_tags_from_message(@data['pull_request']['body'])
+      end
+    end
+
+    # issue番号を取得
+    def number
+      return nil unless @data['action'] == 'created' || @data['action'] == 'opened'
+      if @data.has_key?('issue') #issueの場合
+        @data['issue']['number']
+      elsif @data.has_key?('pull_request') # プルリクの場合
+        @data['pull_request']['number']
+      else
+        nil
+      end
+    end
+
+    # 書き込み内容からタグを生成
+    def create_tags_from_message(message)
+      list = message.scan(/@[a-z0-9_-]+/i)
+      list.map { |v| v.sub('@','') + '_ball' }
+    end
+  end
+
   def self.update_ball_tag(payload)
     self.new(payload).run
   end
 
-  def initialize(payload)
-    @data = JSON.parse(payload)
+  def initialize(arg)
+    @payload = Payload.new(arg)
     @repos = 'kazuhisa/gitball'
     @client = Octokit::Client.new(:access_token => ENV['GITHUB_ACCESS_TOKEN'])
   end
 
   def run
-    return if @data['comment'].blank? || @data['comment']['body'].blank?
-    comment = @data['comment']['body']
-    issue_number = @data['issue']['number']
-    remove_ball_labels(issue_number)
-    tags = create_tags_from_message(comment)
-    add_labels_to_an_issue(issue_number, tags)
+    return if @payload.number.nil?
+    remove_ball_labels(@payload.number)
+    add_labels_to_an_issue(@payload.number, @payload.ball_tags)
   end
 
   # issueにラベルを追加する
@@ -40,11 +74,5 @@ class Github
     labes.select { |v| v[:name] =~ /_ball$/ }.each do |label|
       remove_label(issue_number, label[:name])
     end
-  end
-
-  # 書き込み内容からタグを生成
-  def create_tags_from_message(message)
-    list = message.scan(/@[a-z0-9_-]+/i)
-    list.map { |v| v.sub('@','') + '_ball' }
   end
 end
